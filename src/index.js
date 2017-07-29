cadad7d61b254182fff89cc404448f3fec5c6abd
Ryan Kotzen <potz666@gmail.com>
2017-07-29 19:55:36 +0200

    adding tests.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
 // @flow
 
-import _ from 'lodash/fp/merge';
+import _ from 'lodash';
 import nconf from 'nconf';
 import util from 'util';
-import defaultConfig from './default.js';
+import path from 'path';
 
 /**
- * This function says hello.
- * @param name Some name to say hello to.
- * @returns The hello message.
+ * Initialises nconf using a hierarchy of sources for the source application or script.
+ * The priority order for parameters is [argv, environment variables, source code files]
+ * Also allows for using nconf.set which will save to memory.
+ * @param source string the name of the application or script that we are initialising config for.
  */
-// const sayHello = (name: string = 'World'): string => `Hello, ${name}!`;
-
-function initialiseConfig(source: string) {
+export default function initialiseConfig(source: string) {
     if (!source) {
         throw new Error('Source is required');
     }
-    const envDefault = { NODE_ENV: 'development' };
+    const envDefault = {
+        NODE_ENV: 'development',
+        source: source
+    };
     nconf.argv().env().defaults(envDefault).use('memory'); //lets us call set later on
     const environment = nconf.get('NODE_ENV');
     //eslint-disable-next-line no-process-env
     process.env.NODE_ENV = environment;
     console.log('Environment set to ' + environment);
-    const envFilePath = './' + environment + '.js';
+    const cwd = process.cwd();
     const options = {
         source
     };
-    //eslint-disable-next-line global-require
-    const environmentConfig = require(envFilePath)(options);
+    /* eslint-disable global-require*/
+    const envFilePath = path.join(cwd, 'config/' + environment + '.js');
+    const environmentConfig = require(envFilePath).default(options);
+    const defaultEnvPath = path.join(cwd, 'config/default.js');
+    const defaultConfig = require(defaultEnvPath).default(options);
 
-    _.merge(envDefault, defaultConfig(options), environmentConfig);
+    _.merge(envDefault, defaultConfig, environmentConfig);
     nconf.defaults(envDefault);
     util.inspect.defaultOptions.showHidden = false;
     util.inspect.defaultOptions.depth = 10;
 }
-
-export default initialiseConfig;
--- test/basic.unit.js
+++ test/basic.unit.js
@@ -1,8 +1,22 @@
-import sayHello from '../src';
+import nconf from 'nconf';
 
-describe('sayHello', () => {
-    it('returns hello', () => {
-        expect(sayHello()).toBe('Hello, World!');
-        expect(sayHello('foo')).toBe('Hello, foo!');
+describe('initialiseConfig', () => {
+    it('should merge in the values based on the NODE_ENV value', () => {
+        const initialiseConfig = require('../src').default;
+        const appName = 'someApplication';
+        initialiseConfig(appName);
+        expect(nconf.get('isAwesome')).toBe(true);
+    });
+    it('should should include the source name in the config.', () => {
+        const initialiseConfig = require('../src').default;
+        const appName = 'someApplication';
+        initialiseConfig(appName);
+        expect(nconf.get('source')).toBe(appName);
+    });
+    it('should should include the NODE_ENV value in the config.', () => {
+        const initialiseConfig = require('../src').default;
+        const appName = 'someApplication';
+        initialiseConfig(appName);
+        expect(nconf.get('NODE_ENV')).toBe('test');
     });
 });
